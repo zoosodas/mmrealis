c142d7106bb1a2b66f742fe6b7f73fee0bc398b8
zoosodas <69818219+zoosodas@users.noreply.github.com>
2020-08-17 22:30:44 +0300

    Add files via upload

---
+++ baigiamasis/web/contact.php
@@ -0,0 +1,46 @@
+<section class="contacts" id="contacts">
+    <div class="container">
+        <div class="flex-container">
+            <div class="hide-mobile"></div>
+                <div class="column">
+                    <div class="contactmeh2" >Leave a Message</div>
+                    <form class="contact-form" id="contact" action="index.php" method="post">
+                        <input type="text" name="vardas" placeholder="Name*" required>
+                        <input type="email" name="email" placeholder="Email*" required>
+                        <textarea name="message" rows="5" placeholder="Message*" required></textarea>
+                        <input type="submit" class="hiremeagain" name="submit" value="Hire Me!"> 
+                    </form>
+                </div>
+            <div class="flex-container column">
+                <div class="flex-container mobile-row">
+                    <div class="flex-center">
+                        <i class="fas fa-house-user"></i>
+                    </div> 
+                    <div class="info mobile-row" >
+                        <h3> Location </h3>
+                        <p>324, Golden Tower, Amborkhana, Sylhet </p>
+                    </div>
+                </div>
+                <div class="flex-container mobile-row">
+                    <div class="flex-center">
+                        <i class="far fa-paper-plane"></i>
+                    </div> 
+                    <div class="info mobile-row">
+                        <h3> Email </h3>
+                        <p>junaed@deviserweb.com</p>
+                    </div>
+                </div>
+                <div class="flex-container mobile-row">
+                    <div class="flex-center">
+                        <i class="fas fa-phone-alt"></i>
+                    </div> 
+                    <div class="info">
+                        <h3> Toll Free Call </h3>
+                        <p>0800 234 6578</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+   
---
+++ baigiamasis/web/footer.php
@@ -0,0 +1,15 @@
+<hr>
+<footer class="footer">
+        <div class="container">
+            <nav class="footer-nav">
+                <ul class="flex-container mobile-row">
+                <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fab fa-skype"></i></a></li>
+                <li><a href="#"><i class="fab fa-facebook-square"></i></a></li>
+                </ul>
+                <p>Copyright: Junaed Ahmed, All rights reserved, <?php echo laikas(2020) ?></p>
+            </nav>
+        </div>
+    </footer>
+
+    
